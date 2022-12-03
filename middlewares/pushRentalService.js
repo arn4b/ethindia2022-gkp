4816ca35237fa4400007a05d3108f07ff80b22fe
hrishabh-tykhe <101696562+hrishabh-tykhe@users.noreply.github.com>
2022-12-03 23:45:31 +0530

    QR  Page Dev (#14)
    * progress
    
    * Design update
    
    * chakra changes
    
    * middlewarecalls
    
    * qr code dynamic url

--- middlewares/pushRentalService.js
+++ middlewares/pushRentalService.js
@@ -1,9 +1,10 @@
 import { ethers } from "ethers";
 import abi from "./abi.json";
 import store from "../redux/store";
+import { GKP_CONTRACT_ADDRESS } from "../public/constants";
 
 async function pushRentalService({ name, symbol, limit, price, duration }) {
-  let contractAddress = "0xCAb5B4e33Db07F345b8C10931Dd45ed519A61D3C";
+  let contractAddress = GKP_CONTRACT_ADDRESS;
   const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
