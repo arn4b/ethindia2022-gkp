c6281aeacdd9ce3c0993eb8a7c96790a5323bcea
Arnab Chatterjee <arnabc1701@gmail.com>
2022-12-03 11:11:12 +0530

    added routes for forms

--- components/Common/PrimaryButton.js
+++ components/Common/PrimaryButton.js
@@ -4,7 +4,8 @@ export default function PrimaryButton(props) {
   return (
     <button
       onClick={props.onClick}
-      className="text-lg text-white font-semibold bg-purple-1000 px-5 py-2 shadow-white shadow-sm"
+      // className="text-lg text-white font-semibold bg-purple-1000 px-5 py-2 shadow-white shadow-sm"
+      className="transition ease-in-out delay-50 bg-purple-800 hover:-translate-y-1 hover:scale-110 hover:bg-purple-1000 duration-300 rounded-md text-lg text-white font-semibold px-5 py-2 shadow-sm"
     >
       {props.name}
     </button>
---
+++ pages/sublet-nfts/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Common/Navbar'
+
+
+export default function index() {
+  return (
+
+    <div>
+        <Navbar />
+        Subletting
+    </div>
+  )
+}
