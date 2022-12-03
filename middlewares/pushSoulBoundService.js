1eeb31d07917bb79dc28562744f3d644f3309bdc
hrishabh-tykhe <101696562+hrishabh-tykhe@users.noreply.github.com>
2022-12-03 23:27:27 +0530

    middlewarecalls

--- middlewares/pushSoulBoundService.js
+++ middlewares/pushSoulBoundService.js
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import abi from "./abi.json";
 import store from "../redux/store";
 
-async function pushRentalService({ name, symbol, limit, price, duration }) {
+async function pushSoulBoundGate({ name, symbol, limit, price }) {
   let contractAddress = "0xCAb5B4e33Db07F345b8C10931Dd45ed519A61D3C";
   const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   await provider.send("eth_requestAccounts", []);
@@ -11,9 +11,9 @@ async function pushRentalService({ name, symbol, limit, price, duration }) {
   const contract = new ethers.Contract(contractAddress, abi, signer);
 
   let data = {};
-  data = await contract.createRentedGate(limit, name, symbol);
+  data = await contract.createSoulBoundGate(limit, name, symbol);
 
   return data;
 }
 
-export default pushRentalService;
+export default pushSoulBoundGate;
