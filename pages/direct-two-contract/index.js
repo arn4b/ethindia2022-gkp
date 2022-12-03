b8e99afe48464b32328e0b17fe7901891c296ba2
Arnab Chatterjee <arnabc1701@gmail.com>
2022-12-03 19:34:11 +0530

    org routes added

--- pages/direct-two-contract/index.js
+++ pages/direct-two-contract/index.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Common/Navbar'
 import { Formik, Field, Form } from 'formik';
-import DatePicker from "react-datepicker";
-import { useField, useFormikContext } from "formik";
 import "react-datepicker/dist/react-datepicker.css";
 import PrimaryButton from '../../components/Common/PrimaryButton';
 
 const D2C = () => {
-
-    const [selectedDate, setSelectedDate] = useState(new Date())
     return (
         <div>
             <Navbar />
@@ -17,8 +13,6 @@ const D2C = () => {
                 initialValues={{
                     name: '',
                     symbol: '',
-                    total: '',
-                    selectedDate: selectedDate,
                 }}
                 onSubmit = {
                     (values) => {
@@ -26,23 +20,18 @@ const D2C = () => {
                     }
                 }
             >
-                <Form className='flex flex-col w-3/5'>
-                <label htmlFor="name" className="p-2 mx-4">Name</label>
-                <Field id="name" name="name" placeholder="Jane" className="p-2 rounded-md m-4" />
+                <Form className='flex flex-col justify-center items-center'>
+                    <label htmlFor="name" className="p-2 mx-4">Name</label>
+                    <Field id="name" name="name" placeholder="Jane" className="p-2 rounded-md m-4" />
 
-                <label htmlFor="symbol"  className="p-2 mx-4">Symbol</label>
-                <Field id="symbol" name="symbol" placeholder="MATIC" className="p-2 rounded-md m-4"  />
-
-                <label htmlFor="total"  className="p-2 mx-4">Total</label>
-                <Field id="total" name="total" placeholder="00" className="p-2 rounded-md m-4"  />
-
-                <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} id="selectedDate" name="selectedDate" className="p-2 rounded-md m-4"/>
-                
-                <PrimaryButton
-                    type="submit"
-                    name="Submit"
-                ></PrimaryButton>
-            </Form>
+                    <label htmlFor="symbol"  className="p-2 mx-4">Symbol</label>
+                    <Field id="symbol" name="symbol" placeholder="MATIC" className="p-2 rounded-md m-4"  />
+                    
+                    <PrimaryButton
+                        type="submit"
+                        name="Submit"
+                    ></PrimaryButton>
+                </Form>
             </Formik>
         </div>
     )
--- pages/soul-bound/index.js
+++ pages/soul-bound/index.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Common/Navbar'
 import { Formik, Field, Form } from 'formik';
-import DatePicker from "react-datepicker";
-import { useField, useFormikContext } from "formik";
 import "react-datepicker/dist/react-datepicker.css";
 import PrimaryButton from '../../components/Common/PrimaryButton';
 
 const SoulBound = () => {
 
-    const [selectedDate, setSelectedDate] = useState(new Date())
     return (
         <div>
             <Navbar />
@@ -17,8 +14,8 @@ const SoulBound = () => {
                 initialValues={{
                     name: '',
                     symbol: '',
-                    total: '',
-                    selectedDate: selectedDate,
+                    limit: '',
+                    price: '',
                 }}
                 onSubmit = {
                     (values) => {
@@ -26,24 +23,24 @@ const SoulBound = () => {
                     }
                 }
             >
-                <Form className='flex flex-col w-3/5'>
-                <label htmlFor="name" className="p-2">Name</label>
-                <Field id="name" name="name" placeholder="Jane" className="p-2 rounded-md my-4" />
+                <Form className='flex flex-col justify-center items-center'>
+                    <label htmlFor="name" className="p-2">Name</label>
+                    <Field id="name" name="name" placeholder="Jane" className="p-2 rounded-md my-1" />
 
-                <label htmlFor="symbol"  className="p-2">Symbol</label>
-                <Field id="symbol" name="symbol" placeholder="MATIC" className="p-2 rounded-md my-4"  />
+                    <label htmlFor="symbol"  className="p-2">Symbol</label>
+                    <Field id="symbol" name="symbol" placeholder="MATIC" className="p-2 rounded-md my-1"  />
 
-                <label htmlFor="total"  className="p-2">Total</label>
-                <Field id="total" name="total" placeholder="00" className="p-2 rounded-md my-4"  />
+                    <label htmlFor="limit"  className="p-2">Limit</label>
+                    <Field id="limit" name="limit" type="number" placeholder="00" className="p-2 rounded-md my-1"  />
 
-                <label htmlFor="symbol"  className="p-2">Expiration Date</label>
-                <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} id="selectedDate" name="selectedDate" className="p-2 rounded-md m-4"/>
-                
-                <PrimaryButton
-                    type="submit"
-                    name="Submit"
-                ></PrimaryButton>
-            </Form>
+                    <label htmlFor="price"  className="p-2">Price</label>
+                    <Field id="price" name="price" type="number" placeholder="00" className="p-2 rounded-md my-1"  />
+                    
+                    <PrimaryButton
+                        type="submit"
+                        name="Submit"
+                    ></PrimaryButton>
+                </Form>
             </Formik>
         </div>
     )
