f82f02d372a1443dce67e3a04adb0b86f21e1e14
Arnab Chatterjee <arnabc1701@gmail.com>
2022-12-04 10:57:51 +0530

    changes

--- pages/soul-bound/index.js
+++ pages/soul-bound/index.js
@@ -78,12 +78,31 @@ const SoulBound = () => {
                   returnedGlobalId - 1
                 );
                 localStorage.setItem("contractAddress", finalResult);
-                axios.post(
-                  "https://gkp-push-production.up.railway.app/notifyOrganization",
-                  {
+                // axios.post(
+                //   "https://gkp-push-production.up.railway.app/notifyOrganization",
+                //   {
+                //     organizer_address: userAddress,
+                //   }
+                // );
+                let data = {
                     organizer_address: userAddress,
-                  }
-                );
+                  };
+                  const response = await fetch(
+                    "https://gkp-push-production.up.railway.app/notifyOrganization",
+                    {
+                      method: "POST", // *GET, POST, PUT, DELETE, etc.
+                      mode: "cors", // no-cors, *cors, same-origin
+                      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                      credentials: "same-origin", // include, *same-origin, omit
+                      headers: {
+                        "Content-Type": "application/json",
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                      },
+                      redirect: "follow", // manual, *follow, error
+                      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                      body: JSON.stringify(data), // body data type must match "Content-Type" header
+                    }
+                  );
                 console.log(finalResult);
                 setOpenModal(true);
               } catch (err) {
