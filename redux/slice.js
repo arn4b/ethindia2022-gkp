4c0b61838b573a3d190df4e060012d987ade682f
Aryan Yadav <aryan2019@iiitkottayam.ac.in>
2022-12-04 05:36:36 +0530

    Used wagmi with rainbow kit (#16)

--- redux/slice.js
+++ redux/slice.js
@@ -1,30 +1,33 @@
-
-
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const walletAddressSlice = createSlice({
-    name: 'walletAddress',
-    initialState: {
-        walletAddress: null,
-        // name: null,
-        // home_address: null,
-        // gd_link: null,
-        // age: null,
-        userDetails: null,
-    },
-    reducers: {
-        setAddress: (state, action) => {
-            state.walletAddress = action.payload;
-        },
-        clearAddress: (state) => {
-            state.walletAddress = null;
-        },
-        setUserDetails: (state, action) => {
-            state.userDetails = action.payload;
-        }
-    },
-})
+	name: "walletAddress",
+	initialState: {
+		walletAddress: null,
+		// name: null,
+		// home_address: null,
+		// gd_link: null,
+		// age: null,
+		userDetails: null,
+		secretAdress: null,
+	},
+	reducers: {
+		setAddress: (state, action) => {
+			state.walletAddress = action.payload;
+		},
+		clearAddress: (state) => {
+			state.walletAddress = null;
+		},
+		setUserDetails: (state, action) => {
+			state.userDetails = action.payload;
+		},
+		setSecretAddress: (state, action) => {
+			state.secretAdress = action.payload;
+		},
+	},
+});
 
-export const { setAddress, clearAddress, setUserDetails } = walletAddressSlice.actions
+export const { setAddress, clearAddress, setUserDetails, setSecretAddress } =
+	walletAddressSlice.actions;
 
-export default walletAddressSlice.reducer
+export default walletAddressSlice.reducer;
