4810bca6a1901b66d92915f684fecc7f73cacdb6
Arnab Chatterjee <arnabc1701@gmail.com>
2022-12-04 05:36:55 +0530

    css fixes

--- pages/users/renting.js
+++ pages/users/renting.js
@@ -7,10 +7,10 @@ const UserWhitelistedContracts = () => {
   return (
     <div className="bg-black">
       <Navbar />
-      <div className="mx-5 text-white text-2xl font-semibold">My SBTs</div>
-      <div className="grid grid-cols-3 grid-rows-1">
+      <div className="m-5 mx-10 text-white text-4xl font-semibold font-fragile">My SoulBound Tokens</div>
+      <div className="grid grid-cols-4 grid-rows-1 justify-items-center items-center gap-16">
         {l.map((key, idx) => {
-          return <ItemTile isRent />;
+          return <ItemTile />;
         })}
       </div>
     </div>
